bf22af884da48b6018df308da7391f0c5000bb04
Bhairav <bhairavmehta95@gmail.com>
2016-08-01 09:32:30 +0000

    Device_id + install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
-git clone https://github.com/sarfata/pi-blaster
+sudo apt-get install bluetooth
+sudo apt-get install python-bluez
 sudo apt-get install autoconf
-Y
+pip install requests
+git clone https://github.com/sarfata/pi-blaster
 cd pi-blaster
 ./autogen.sh
 ./configure
 make
 cd ..
-sudo apt-get install bluetooth
-sudo apt-get install python-bluez
 cd 
 cd 3M_LED_Pi
 python raspi_code.py
